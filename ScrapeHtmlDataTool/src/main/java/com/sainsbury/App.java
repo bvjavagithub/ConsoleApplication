3d4a007ed26c11ec7143d89c00f221f72fc6cc2e
bvjavagithub <bvjavagithub@gmail.com>
2016-05-08 00:25:27 +0100

    Final commit.

--- ScrapeHtmlDataTool/src/main/java/com/sainsbury/App.java
+++ ScrapeHtmlDataTool/src/main/java/com/sainsbury/App.java
@@ -1,13 +1,51 @@
 package com.sainsbury;
 
-/**
- * Hello world!
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sainsbury.dao.OutputHelper;
+import com.sainsbury.exception.SainsburyException;
+import com.sainsbury.model.Results;
+import com.sainsbury.utility.SainsburyConstants;
+
+/** 
+ * @author Bhakti
+ * 
+ * This is the entry point for the application. To make an application specific for particular html link no need to pass an argument from command line.
+ * To make this application generic for any html link to scrape data, need to pass a main method argument html link from command line. Need to make sure, the 
+ * structure of web page is same for any link, as code looks for specific elements for e.g. div element "productInfo" etc.
  *
  */
+
 public class App 
 {
+	final static Logger logger = Logger.getLogger(App.class); // Logger to log messages and error.
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	logger.info("Start of the application.....");
+    	ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {SainsburyConstants.SPRING_BEANS_XML});
+
+    	OutputHelper output = (OutputHelper)context.getBean(SainsburyConstants.OUTPUT_HELPER);
+    	Results results = null;
+    	
+    	try {
+    		if(args.length==0){
+    			// Scrape html data for a given Sainsbury product link.
+    			results = output.getResults("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html");
+    		}
+    		else{
+    			// To make application generic, pass the html link argument from command line. Scrape data for any html link with the same web page structure.
+        		 results = output.getResults(args[0]);
+    		}    		
+			logger.info("Got the product list.... ");
+			logger.info("JSON Output: "+output.getJsonOutput(results));
+		} catch (IOException | JSONException | SainsburyException ex) {
+			logger.error("Error while processing data : ", ex);
+		} 
     }
 }
--- ScrapeHtmlDataTool/src/test/java/com/sainsbury/AppTest.java
+++ ScrapeHtmlDataTool/src/test/java/com/sainsbury/AppTest.java
@@ -1,38 +1,137 @@
 package com.sainsbury;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;import org.junit.Before;
+import org.junit.Test;
+
+import com.sainsbury.exception.SainsburyException;
+import com.sainsbury.model.Results;
+import com.sainsbury.service.impl.JsonServiceImpl;
+import com.sainsbury.service.impl.ScrapeHtmlDataServiceImpl;
+
+import static org.junit.Assert.*;
 
 /**
- * Unit test for simple App.
+ * Unit tests for project ScrapeHtmlDataTool.
  */
 public class AppTest 
-    extends TestCase
+    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+   String htmlLink="http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html";	// Sainsbury product link given in assignment to test.
+   Document doc;
+   ScrapeHtmlDataServiceImpl scrapeHtmlDataImpl;
+   JsonServiceImpl jsonServiceImpl;
+   
+   /**
+    * This is set up method to create objects once to use in all the tests.
+    * @throws IOException
+    */
+   @Before
+   public void setUp() throws IOException{
+	   doc = Jsoup.connect(htmlLink).get();
+	   scrapeHtmlDataImpl = new ScrapeHtmlDataServiceImpl();
+	   jsonServiceImpl = new JsonServiceImpl();
+   }
+  
+   /**
+    * To test the loaded document object is not null.
+    */
+   @Test
+   public void testDocumentNotNull(){
+	  assertNotNull(doc);
+   }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+   /**
+    * Test the product title.
+    * @throws IOException
+    */
+   @Test
+   public void testProductTitle() throws IOException{
+		doc = Jsoup.connect("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/sainsburys-apricot-ripe---ready-320g.html").get();
+		String title = scrapeHtmlDataImpl.getProductTitle(doc);
+		assertNotNull(title);
+		assertEquals(title, "Sainsbury's Apricot Ripe & Ready x5");
+   }   
+  
+   /**
+    * Test the product unit price.
+    * @throws IOException
+    */
+   @Test
+   public void testProductUnitPrice() throws IOException{
+		doc = Jsoup.connect("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/sainsburys-golden-kiwi--taste-the-difference-x4-685641-p-44.html").get();
+		double unitPrice = scrapeHtmlDataImpl.getProductPricePerUnit(doc);
+		String unitPriceStr=String.valueOf(unitPrice);
+		assertEquals(unitPriceStr, "1.8");
+   }
+   
+   /**
+    * Test the product description.
+    * @throws IOException
+    */
+   @Test
+   public void testProductDescription() throws IOException{
+		doc = Jsoup.connect("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/sainsburys-conference-pears--ripe---ready-x4-%28minimum%29.html").get();
+		String description = scrapeHtmlDataImpl.getProductDescription(doc);
+		assertNotNull(description);
+		assertEquals(description, "Conference");
+   }
+   
+   /**
+    * Test the size of linked html.
+    * @throws IOException
+    */
+   @Test
+   public void testLinkedHTMLSize() throws IOException{
+		doc = Jsoup.connect("http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/sainsburys-conference-pears--ripe---ready-x4-%28minimum%29.html").get();
+		String size = scrapeHtmlDataImpl.getProductSize(doc);
+		assertNotNull(size);
+		assertEquals(size, "35kb");
+   }
+   
+   /**
+    * Test the results object is not null.
+    * @throws IOException
+    * @throws SainsburyException
+    */
+   @Test
+   public void testResultsNotNull() throws IOException, SainsburyException{
+	   assertNotNull(scrapeHtmlDataImpl.getResults(htmlLink));
+   }
+   
+   /**
+    * Test the total unit price.
+    * @throws IOException
+    * @throws SainsburyException
+    */
+   @Test
+   public void testTotalUnitPrice() throws IOException, SainsburyException{
+	 Results results = scrapeHtmlDataImpl.getResults(htmlLink);
+	 String totalStr = String.valueOf(results.getTotal());
+	 assertEquals(totalStr, "15.100000000000001");
+   }
+   
+   /**
+    * Test the final JSON output.
+    * @throws IOException
+    * @throws JSONException
+    * @throws SainsburyException
+    */
+   @Test
+   public void testJSONOutputNotNull() throws IOException, JSONException, SainsburyException{
+	   Results results = scrapeHtmlDataImpl.getResults(htmlLink);
+	   JSONObject jObject = jsonServiceImpl.getJsonOutput(results);
+	   assertNotNull(jObject);
+   }
+   
+   
+   
+   
+   
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+   
 }
